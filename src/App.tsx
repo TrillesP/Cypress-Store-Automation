543fba65c414f799414f259fc80dcd2426bb2edf
Gabriel Fatori <gabriel.fatori@gmail.com>
2022-07-17 20:40:10 -0300

    initial commit, with some basic structure.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
