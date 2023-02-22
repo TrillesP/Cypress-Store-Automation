2a4c5c84361424d3b0278b5ea35c5f3bc549847a
Pedro Trilles Machado <trillessky@gmail.com>
2023-02-22 14:55:58 -0300

    updated selectors

--- cypress/fixtures/selectors.js
+++ cypress/fixtures/selectors.js
@@ -4,9 +4,17 @@ export const selectors = {
     goToCartButton: '.sc-hKMtZM',
     addToCartButton: '.sc-hlnMnd',
     removeItem: '.sc-cCsOjp',
+    confirmCheckout: '.sc-cxabCf',
   },
   inputs: {
-
+    inputSection: '.sc-kDDrLX',
+    ZIP: '.sc-crXcEl',
+    address: '.sc-evZas',
+    num: '.sc-ksZaOG',
+    additionalInfo: '.sc-hAZoDl',
+    neighborhood: '.sc-fEOsli',
+    city: '.sc-bjUoiL',
+    state: '.sc-idiyUo',
   },
   values: {
     productsInCart: '.sc-eCYdqJ',
