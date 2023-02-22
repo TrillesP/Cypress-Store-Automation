f1b3041aea0f0bb5e785876e272422fe963f87fb
Pedro Trilles Machado <trillessky@gmail.com>
2023-02-22 15:17:12 -0300

    optimized comments

--- cypress/e2e/2-paginaCheckoutESuccess/pageCheckoutAndSuccess.cy.js
+++ cypress/e2e/2-paginaCheckoutESuccess/pageCheckoutAndSuccess.cy.js
@@ -3,27 +3,28 @@
 import { selectors } from '../../fixtures/selectors'
 
 describe('testes de elementos e botões da página e Checkout e Checkout Success', () => {
+
   beforeEach(() => {
     cy.visit('http://localhost:5173/checkout/')
   })
 
   it('verifica existência dos 7 inputs, seus placeholders e botão de confirmar pedido', () => {
-    cy.get(selectors.inputs.inputSection).find('input').should('have.length', 7)
+    cy.get(selectors.inputs.inputSection).find('input').should('have.length', 7);
     cy.get(selectors.inputs.ZIP)
       .invoke('attr', 'placeholder')
-      .should('eq', 'CEP')
+      .should('eq', 'CEP');
     cy.get(selectors.inputs.address)
       .invoke('attr', 'placeholder')
-      .should('eq', 'Rua')
+      .should('eq', 'Rua');
     cy.get(selectors.inputs.num)
       .invoke('attr', 'placeholder')
-      .should('eq', 'Número')
+      .should('eq', 'Número');
     cy.get(selectors.inputs.additionalInfo)
       .invoke('attr', 'placeholder')
       .should(
         'eq',
         'Complemento                                                 Opcional',
-      )
+      );
     cy.get(selectors.inputs.neighborhood)
       .invoke('attr', 'placeholder')
       .should('eq', 'Bairro')
