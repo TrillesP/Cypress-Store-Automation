f17e742d0023818584250911c597d47f70cdd3ad
Pedro Trilles Machado <trillessky@gmail.com>
2023-02-22 14:00:25 -0300

    added file to store all selectors

---
+++ cypress/fixtures/selectors.js
@@ -0,0 +1,22 @@
+export const selectors = {
+  buttons: {
+    homeButton: '[href="/"] > img',
+    goToCartButton: '.sc-hKMtZM',
+    addToCartButton: '.sc-hlnMnd',
+    removeItem: '.sc-cCsOjp',
+  },
+  inputs: {
+
+  },
+  values: {
+    productsInCart: '.sc-eCYdqJ',
+    totalValue: '.sc-llJcti',
+  },
+  e: {
+    allProducts: '.sc-eKBdFk',
+    allCoffees: '.sc-GVOUr',
+    addOrRemove: '.sc-ZyCDH',
+    addOrRemoveCheckout: '.sc-gXmSlM',
+    infoCart: '.sc-iIPllB',
+  },
+}
